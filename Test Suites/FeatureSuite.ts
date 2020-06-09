6aa7a9f4b3dd1c994cca8bf92ee3c68e163490e7
Safa Fekih Hassen <safafekihhassen@gmail.com>
2020-06-09 15:03:34 +0100

    featureFileTc

---
+++ Test Suites/FeatureSuite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>FeatureSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>75b11545-431c-4a4d-87db-f294800e6394</testSuiteGuid>
+   <testCaseLink>
+      <guid>587b1e6c-6123-4054-b7f5-0f6da489441d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RunFeatureFiles</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/FirstTestSuit.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>FirstTestSuit</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6e78f5cd-1b3e-4b3a-b9ea-7793815e6436</testSuiteGuid>
+   <testCaseLink>
+      <guid>6ccc1199-2970-4bce-ae57-e278c8d71047</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/cartTC</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>abbe0610-8f42-44c4-b666-38b559480459</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/loginTC</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>33a43f92-84b2-428d-9d9b-ea7c3baba1b3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/orderTC</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bb8e02b2-0c36-47da-9dc4-0136088b3558</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/registerTC</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c90af489-14a9-4c20-af31-700b032e1c23</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/shopTC</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
