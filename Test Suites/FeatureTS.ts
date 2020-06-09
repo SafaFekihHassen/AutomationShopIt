2c5d8529a51b99b4ee5cf4f34d13c692fbea9ce1
Safa Fekih Hassen <safafekihhassen@gmail.com>
2020-06-09 16:37:07 +0100

    featureCommit

---
+++ Test Suites/FeatureTS.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>FeatureTS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>facfe166-693e-4209-a404-89e216cf7b55</testSuiteGuid>
+   <testCaseLink>
+      <guid>035ff73b-6f4e-4e97-8510-a6eda70eaddf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/FeatureTC</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
